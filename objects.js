4d778d510b0d0afa95b1a0aec8365fe8f6a5ebb6
Harum Helmy <harumendhah@gmail.com>
2019-04-30 20:09:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -9,6 +9,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
+updatePlaylist(playlist, 'Phil Ochs', '"Here\'s to the State of Mississippi"')
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName
   return playlist
