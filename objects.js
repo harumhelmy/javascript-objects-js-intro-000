8b75b456cf56fb367483f372cc510217b5d764b4
Harum Helmy <harumendhah@gmail.com>
2019-04-30 20:18:36 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,12 +4,12 @@ var playlist = {
   'Kacey Musgraves': "'Slow Burn'"
 };
 
-function updatePlaylist(playlist, artistName, songTitle) {
+function updatePlaylist (playlist, artistName, songTitle) {
   playlist.artistName = songTitle
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
+function removeFromPlaylist (playlist, artistName) {
   delete playlist.artistName
   return playlist
 }
