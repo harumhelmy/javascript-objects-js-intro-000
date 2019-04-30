9ce8d32f78f7c3dd8f01332f542f4114b581a7f0
Harum Helmy <harumendhah@gmail.com>
2019-04-30 19:50:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,13 +3,3 @@ var playlist = {
   Clipse: "'Grindin'",
   Kacey Musgraves: "'Slow Burn'"
 };
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
-  return playlist
-}
-
-function removeFromPlaylist (playlist, artistName) {
-  delete playlist.artistName;
-  return playlist
-}
